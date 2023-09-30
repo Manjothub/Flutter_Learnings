52a56cd68f2117d22568cff4fd2c84fbd9a8b7a6
Manjot Singh <heyitsmanjotsingh@gmail.com>
2023-09-30 16:43:33 +0530

    Routes and Functions Learning Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_basics/home_page.dart';
+import 'package:flutter_basics/pages/home_page.dart';
+import 'package:flutter_basics/pages/login_page.dart';
 void main(){
   runApp(MyApp());
 }
@@ -8,7 +9,17 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      home: Homepage(),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(primarySwatch: Colors.green),
+      // darkTheme: ThemeData(
+      //   brightness: Brightness.dark
+      // ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context)=> LoginPage(),
+        "/home":(context)=>Homepage(),
+        "/login":(context)=> LoginPage()
+      },
     );
   }
 }
